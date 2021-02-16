64e64b6ca591b26e463e1be842950fab4c48277b
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2021-02-17 00:03:12 +0530

    Updated App to handle CRUD operations on the static database Proxy class

--- src/main/java/com/golden/jaxRSBasic/resources/MessageResource.java
+++ src/main/java/com/golden/jaxRSBasic/resources/MessageResource.java
@@ -2,7 +2,6 @@ package com.golden.jaxRSBasic.resources;
 
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
--- src/main/java/com/golden/jaxRSBasic/service/MessageService.java
+++ src/main/java/com/golden/jaxRSBasic/service/MessageService.java
@@ -5,23 +5,42 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import com.golden.jaxRSBasic.database.DatabaseClass;
 import com.golden.jaxRSBasic.model.Message;
 
 public class MessageService {
 	
 	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1,"Hello World!",new Date(),"Sreek"));
+		messages.put(2L, new Message(2,"Hello Jersey Jax-RS!",new Date(),"Sreek"));
+	}
 
 	public List<Message> getAllMessages() {
-		List<Message> messages = new ArrayList<>();
-		
-		messages.add(new Message(1, "Test", new Date(), "Test 1"));
-		messages.add(new Message(2, "Test2", new Date(), "Test 2"));
-		messages.add(new Message(3, "Test3", new Date(), "Test 3"));
-		
-		return messages;
+		return new ArrayList<Message>(messages.values());
 	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if(message.getId()<=0)
+			return null;
+		messages.put(message.getId(), message);
+		return messages.get(message.getId());
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
+	
 
 }
