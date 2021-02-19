e561b550e57111c24390869428defec43ff6abfe
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2021-02-19 22:49:37 +0530

    Added delete endpoint in the Resource class

--- src/main/java/com/golden/jaxRSBasic/resources/MessageResource.java
+++ src/main/java/com/golden/jaxRSBasic/resources/MessageResource.java
@@ -2,8 +2,11 @@ package com.golden.jaxRSBasic.resources;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,14 +15,21 @@ import com.golden.jaxRSBasic.service.MessageService;
 
 @Path("/messages")
 public class MessageResource {
-	
-	
+
 	MessageService service = new MessageService();
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getAllMessages() {
 		return service.getAllMessages();
+	}
+	
+
+	@DELETE
+	@Path("/remove/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message removeMessage(@PathParam("id") Long id) {
+		return service.removeMessage(id);
 	}
 
 }
