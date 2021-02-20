ea0b7362408bd097335488b37e89ac1e6dc29a57
Sreekar Swarnapuri <sreekar.swarnapuri@hotmail.com>
2021-02-20 18:57:06 +0530

    - Updated Jackson conversion dependency - Added ADD, DELETE and UPDATE messages end points

--- src/main/java/com/golden/jaxRSBasic/resources/MessageResource.java
+++ src/main/java/com/golden/jaxRSBasic/resources/MessageResource.java
@@ -2,8 +2,10 @@ package com.golden.jaxRSBasic.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -31,7 +33,23 @@ public class MessageResource {
 		return service.getMessage(id);
 	}
 	
-
+	@POST
+	@Path("/addMessage")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return service.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("id") Long id, Message message) {
+		message.setId(id);
+		return service.updateMessage(message);
+	}
+	
 	@DELETE
 	@Path("/remove/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
